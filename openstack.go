b3a8ea2802d8d1888fe0cc6afc07d4a404e6dd1c
yogetter <getter@inwinstack.com>
2018-05-29 02:10:45 +0000

    Rename function & update keystone V3 API

--- openstack.go
+++ openstack.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -12,68 +11,75 @@ import (
 )
 
 type openstackConf struct {
-	Tenantname  string
-	Username    string
-	Password    string
 	OS_AUTH_URL string
+	NOVA_ENDPOINT string
 }
 
-func (o *openstackConf) init() {
+func (o *openstackConf) Init() {
 	data, _ := os.Open("openstack_conf.json")
 	decoder := json.NewDecoder(data)
 	err := decoder.Decode(o)
-	checkError(err)
+	CheckError(err)
 }
 
-func (o *openstackConf) getTokenUrl(json_data []byte) (string, string) {
-	var tmp interface{}
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", o.OS_AUTH_URL+"/tokens", bytes.NewBuffer(json_data))
-	checkError(err)
+func (o *openstackConf) GetUrl(catalog interface{}) {
+	for _, value := range catalog.([]interface{}){
+		if value.(map[string]interface{})["name"] == "nova" {
+			for _, url := range value.(map[string]interface{})["endpoints"].([]interface{}) {
+				if url.(map[string]interface{})["interface"] == "internal" {
+					o.NOVA_ENDPOINT = url.(map[string]interface{})["url"].(string)
+				}
+			}
+		}
+	}
+}
 
+func (o *openstackConf) GetToken() (string) {
+	var ResponseData interface{}
+	data, _ := os.Open("user_info.json")
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", o.OS_AUTH_URL+":5000/v3/auth/tokens", data)
+	CheckError(err)
 	req.Header.Set("Content-Type", "application/json")
 	res, err := client.Do(req)
-
 	defer res.Body.Close()
-	o.ioRead(res, &tmp)
-
-	res_data := tmp.(map[string]interface{})["access"].(map[string]interface{})
-	url := res_data["serviceCatalog"].([]interface{})[0].(map[string]interface{})["endpoints"].([]interface{})[0].(map[string]interface{})["adminURL"]
-	token := res_data["token"].(map[string]interface{})["id"]
-	return token.(string), url.(string)
+	o.IoRead(res, &ResponseData)
+	catalog := ResponseData.(map[string]interface{})["token"].(map[string]interface{})["catalog"]
+	o.GetUrl(catalog)
+	token := res.Header.Get("X-Subject-Token")
+	return token
 }
-func (o *openstackConf) ioRead(r *http.Response, f *interface{}) {
+func (o *openstackConf) IoRead(r *http.Response, f *interface{}) {
 	body, err := ioutil.ReadAll(r.Body)
 	dec := json.NewDecoder(strings.NewReader(string(body)))
 	err = dec.Decode(f)
-	checkError(err)
+	CheckError(err)
 
 }
-func (o *openstackConf) insertInstance(res_data []interface{}) []string {
+func (o *openstackConf) InsertInstance(res_data []interface{}) []string {
 	var tmp []string
 	for _, value := range res_data {
 		tmp = append(tmp, value.(map[string]interface{})["id"].(string))
 	}
 	return tmp
 }
-func (o *openstackConf) getInstances(influx *db) []interface{} {
-	var json_data = []byte(`{"auth":{"tenantName":"` + o.Tenantname + `","passwordCredentials":{"username":"` + o.Username + `", "password":"` + o.Password + `"}}}`)
+func (o *openstackConf) GetInstances(influx *db) []interface{} {
 	var tmp interface{}
-	token, url := o.getTokenUrl(json_data)
-
+	token := o.GetToken()
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", url+"/servers", nil)
-	checkError(err)
+	req, err := http.NewRequest("GET", o.NOVA_ENDPOINT + "/servers?all_tenants", nil)
+	CheckError(err)
 	req.Header.Set("X-Auth-Token", token)
 	res, err := client.Do(req)
 	defer res.Body.Close()
-	o.ioRead(res, &tmp)
-
+	o.IoRead(res, &tmp)
+	log.Println(tmp)
 	res_data := tmp.(map[string]interface{})["servers"].([]interface{})
+	log.Println(res_data)
 	return res_data
 }
 
-func deleteData(influx *db) {
+func DeleteData(influx *db) {
 	// Found ID should be delete
 	flag := true
 	for _, data1 := range db_instances {
@@ -92,7 +98,7 @@ func deleteData(influx *db) {
 		flag = true
 	}
 }
-func queryData(influx *db) {
+func QueryData(influx *db) {
 	tmp_data := influx.queryInfo("uuid", "show tag values from vm_usage with key = ")
 	if tmp_data[0].Series != nil {
 		//log.Println("Tmp_data:", tmp_data[0], "value:", tmp_data[0].Series)
@@ -100,7 +106,7 @@ func queryData(influx *db) {
 	}
 }
 
-func checkError(err error) {
+func CheckError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -113,16 +119,16 @@ var influx db
 
 func main() {
 	influx := db{}
-	influx.init()
+	influx.Init()
 	run = openstackConf{}
-	run.init()
+	run.Init()
 	for {
-		rep_data := run.getInstances(&influx)
-		live_instances = run.insertInstance(rep_data)
-		queryData(&influx)
+		rep_data := run.GetInstances(&influx)
+		live_instances = run.InsertInstance(rep_data)
+		QueryData(&influx)
 		log.Println("InDb:", db_instances)
 		log.Println("Live:", live_instances)
-		deleteData(&influx)
+		DeleteData(&influx)
 		time.Sleep(60 * time.Second)
 	}
 }
